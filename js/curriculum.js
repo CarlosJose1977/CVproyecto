78467e2f0029504b0572a5a225a1d80b4796f041
Carlos Gomez <cjgomez77@hotmail.com>
2023-01-30 13:22:19 -0300

    sintaxis correcta de JS para botones de referencias.

--- js/curriculum.js
+++ js/curriculum.js
@@ -1,14 +1,23 @@
 
-
-const open = document.getElementById('open');
-const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
-
-open.addEventlistener('click', () => {
-    modal_container.classList.add('show');
+document.getElementById('open').addEventListener('click', function () {
+    console.log("capturamos el evento click open");
+    document.getElementById("modal_container").classList.add('show');
 });
 
-close.addEventlistener('click', () => {
-    modal_container.classList.remove('show');
+document.getElementById('close').addEventListener('click', function () {
+    console.log("capturamos el evento click close");
+    document.getElementById("modal_container").classList.remove('show');
 });
+
+
+//const modal_container = document.getElementById('modal_container');
+//const close = document.getElementById('close');
+
+//open.addEventlistener('click', () => {
+//   modal_container.classList.add('show');
+//});
+
+//close.addEventlistener('click', () => {
+//    modal_container.classList.remove('show');
+//});
 
